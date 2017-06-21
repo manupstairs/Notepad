22ccd48ca1b8d6885058106d4ae5c072340b06e6
manupstairs <manupstairs@163.com>
2017-06-21 22:02:05 +0800

    Add hello world project.

---
+++ Notepad/HelloMVVM/HelloModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloMVVM
+{
+    public class HelloModel
+    {
+        public string HelloWorld { get; set; } = "Hello World";
+    }
+}
---
+++ Notepad/HelloMVVM/HelloViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloMVVM
+{
+    public class HelloViewModel
+    {
+        public HelloModel HelloModel { get; set; } = new HelloModel();
+    }
+}
