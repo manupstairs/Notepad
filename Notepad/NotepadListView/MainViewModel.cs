b3a8e6f163d73972af4297846acfd604a8563780
manupstairs <manupstairs@163.com>
2017-06-22 22:02:02 +0800

    Add ListView project

---
+++ Notepad/NotepadListView/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotepadListView
+{
+    public class MainViewModel
+    {
+        public List<NotepadPage> Pages { get; set; } = new List<NotepadPage>();
+
+        public List<string> Titles { get; set; } = new List<string>();
+
+        public MainViewModel()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var title = "Title" + i;
+                Titles.Add(title);
+
+                var page = new NotepadPage
+                {
+                    Title = title
+                };
+                Pages.Add(page);
+            }
+        }
+    }
+}
---
+++ Notepad/NotepadListView/NotepadPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotepadListView
+{
+    public class NotepadPage
+    {
+        public string Title { get; set; } 
+    }
+}
