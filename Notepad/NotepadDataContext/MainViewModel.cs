3328c142cfeca571f69e09f762f9cae4474a2afb
manupstairs <manupstairs@163.com>
2017-06-24 22:06:35 +0800

    Add DataContext project

---
+++ Notepad/NotepadDataContext/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotepadDataContext
+{
+    public class MainViewModel
+    {
+        public string Name { get; set; } = "My name is MainViewModel";
+    }
+}
