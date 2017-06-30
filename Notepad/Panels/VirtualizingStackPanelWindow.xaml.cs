ddce975aca686689d7c2a7227914667bdea8eec9
manupstairs <manupstairs@163.com>
2017-06-30 22:14:17 +0800

    Update Panels project

---
+++ Notepad/Panels/VirtualizingStackPanelWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Panels
+{
+    /// <summary>
+    /// Interaction logic for VirtualizingStackPanelWindow.xaml
+    /// </summary>
+    public partial class VirtualizingStackPanelWindow : Window
+    {
+        public List<int> Items { get; set; } = new List<int>();
+
+        public VirtualizingStackPanelWindow()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                Items.Add(i);
+            }
+            this.DataContext = this;
+        }
+    }
+}
