22b2bd648e8d2f48328dfaf8bdd7cedf8cafd739
manupstairs <manupstairs@163.com>
2017-07-03 22:41:33 +0800

    Add UsageOf Converter

---
+++ Notepad/UsageOfConverter/BoolToString.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace UsageOfConverter
+{
+    public class BoolToString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? "YES" : "NO";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var yesOrNo = value.ToString();
+            if (yesOrNo.Equals("YES", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class StringToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Enum.Parse(typeof(Visibility), value.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Notepad/UsageOfConverter/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsageOfConverter
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string visibilitySatus;
+
+        public string VisibilitySatus
+        {
+            get { return visibilitySatus; }
+            set {
+                visibilitySatus = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public List<string> Items { get; set; }
+
+        public MainViewModel()
+        {
+            Items = new List<string>
+            {
+                "Visible","Hidden","Collapsed"
+            };
+            VisibilitySatus = "Visible";
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
